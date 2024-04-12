e1683c48674d3d136e99ff151d0a7951a0d1e412
senicko <25861704+senicko@users.noreply.github.com>
2024-04-12 21:34:54 +0200

    First commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const dateFormatter = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "medium",
+});
